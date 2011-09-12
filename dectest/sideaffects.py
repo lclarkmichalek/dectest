8c273e3aef2e1a1fe9d30b7bb39bb9b0b8aaabc5
Laurie Clark-Michalek <bluepeppers@archlinux.us>
2011-09-12 09:07:42 +0100

    Added GlobalStateChange sideaffect test

--- dectest/sideaffects.py
+++ dectest/sideaffects.py
@@ -32,3 +32,38 @@ class SideAffectTest():
         A decorator that does nothing.
         """
         return func
+
+class GlobalStateChange(SideAffectTest):
+    """
+    A side affect test for changes in global state.
+    """
+    name = "globalstatechange"
+    
+    func = None
+    change = {}
+    
+    def test(self):
+        """
+        Returns true if the `self.func.__globals__` is equal to 
+        `self.initglobal.update(self.expectedglobal)`.
+        """
+        glob = self.func.__globals__
+        for key in self.change:
+            if self.change[key] != glob[key]:
+                return False
+        return True
+    
+    def decorator(self, change):
+        """
+        Takes a dict of items that should be in the global state, and stores
+        them for the :method:`test` to check.
+        """
+        self.change = change
+        def dec(func):
+            """
+            Get the function, so as to retrieve the global state later.
+            """
+            self.func = func
+            return func
+        
+        return dec
