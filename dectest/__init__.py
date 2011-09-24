24d03dd7a35aa5bf144356fa4b322bc6d9bf3cf1
Laurie Clark-Michalek <bluepeppers@archlinux.us>
2011-09-24 20:35:01 +0100

    Added some content to the __init__.py file, along with a lot more sphinx optimised documentation.

--- dectest/__init__.py
+++ dectest/__init__.py
@@ -1,4 +1,11 @@
 """
-The base dectest package. The only class that should be needed to be imported is
-:class:`TestSuite`.
+The base dectest package. Provides a shortcut to several usefull classes. The
+:class:`~.suite.TestSuite` class is imported, as it is vital to any use of
+dectest. Two different config methods are imported: :class:`~.config.DictConfig`
+and :class:`~.config.PythonFileConfig`, along with two side affect tests from
+the :mod:`~.sideaffects` module: :class:`~.sideaffects.GlobalStateChange` and
+:class:`~.sideaffects.ClassStateChange`.
 """
+from .config import DictConfig, PythonFileConfig
+from .suite import TestSuite
+from .sideaffects import GlobalStateChange, ClassStateChange
